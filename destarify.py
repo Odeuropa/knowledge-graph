c85b5a1e5cd9d7409f8e86dc769eedc812df2fae
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2023-09-21 15:19:35 +0200

    v2 for italian corpora

---
+++ destarify.py
@@ -0,0 +1,21 @@
+'''This code removes the RDF-star part'''
+import os
+import re
+from tqdm import tqdm
+
+source = './dump'
+dest = './dump-flat'
+
+for folder in tqdm(sorted(os.listdir(source))):
+    if not os.path.isdir(os.path.join(source, folder)):
+        continue
+    for file in os.listdir(os.path.join(source, folder)):
+        if not file.endswith('.ttl'):
+            continue
+        with open(os.path.join(source, folder, file)) as f:
+            orig = f.read()
+
+        final = re.sub(r'<< (.+) >> .+\.', r'\1 .', orig, flags=re.MULTILINE)
+        os.makedirs(os.path.join(dest, folder), exist_ok=True)
+        with open(os.path.join(dest, folder, file), 'w') as f:
+            f.write(final)
